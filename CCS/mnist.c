a2f861b740dca05ac5045913590f973ac4aa0a4b
Paul Dutton <jodgey4@gmail.com>
2025-05-10 13:08:01 -0500

    Delete mnist.c

--- CCS/mnist.c
+++
@@ -1,9 +0,0 @@
-#include "mnist.h"
-
-#pragma diag_suppress=1544 // loop counting up
-#pragma diag_suppress=1545 // int vs unsigned int array access
-
-// deprecated
-int getNext(image img) {
-	return 42;
-}
